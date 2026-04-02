e7e1870da639d5c1ad8612c44ab7580bbcb7994a
OhSeungY <150667559+OhSeungY@users.noreply.github.com>
2026-04-02 17:49:16 +0900

    Update 2-4.c

--- code/2-4/2-4.c
+++ code/2-4/2-4.c
@@ -7,11 +7,14 @@ int main() {
 
     for(int i=0; i<n; i++){
         char ch = chars[i];
-        printf("%c - alpha:%d digit:%d upper:%c lower:%c\n", ch, 
+        printf("%c - alpha:%d digit:%d upper:%c lower:%c", ch, 
             isalpha(ch), 
             isdigit(ch), 
             toupper(ch), 
             tolower(ch));
+        if(i < n-1){
+            printf("\n");
+        }
     }
 
     return 0;
